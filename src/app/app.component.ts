c8a92a4d34a70af56c8c5c4eba500ad68b092124
pascal dumas <pdumas@stagiaire-humanbooster.com>
2021-01-27 00:12:33 +0100

    nouvel ajout travail du soir

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'revisionavecwilliam';
+  
+  public isStarted = false;  
+  public isPause = false;
+  public time = 0;
+  private idInterval : null | number = null;
+  
+  private startChrono() : void{
+    this.idInterval = setInterval(()=>{
+    this.time++;
+      } , 1);        
+    }
+  public pauseChrono() : void{
+    clearInterval(this.idInterval);
+    this.isPause = true;
+    
+  }
+  public startSwitch() : void{
+    this.isStarted = true;
+    this.startChrono();
+    this.isPause = false;
+  }
+  public effacerChrono() : void{
+    this.time = 0;
+    this.isStarted = false;
+    
+  }
 }
